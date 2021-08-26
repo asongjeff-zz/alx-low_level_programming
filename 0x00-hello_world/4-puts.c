4feaa07f3be2807bccd7d7a7fd99726ff93b50ff
asongjeff <asongjeff@gmail.com>
2021-08-26 16:25:11 +0100

    Added Files for 0x00-hello_world mandatory tasks

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Use Puts to display a line of text
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Use Printf to display a line of text
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
