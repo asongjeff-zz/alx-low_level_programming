38505856c812e1420f6a65c789b58f709b90014b
asongjeff <asongjeff@gmail.com>
2021-08-27 19:14:16 +0100

    Added 101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Print all possible different combinations of 3 digits.
+ * Write a program that prints all possible different combinations of three digits.
+ * Numbers must be separated by ,, followed by a space
+ * The three digits must be different
+ * 012, 120, 102, 021, 201, 210 are considered the same combination of the three digits 0, 1 and 2
+ * Print only the smallest combination of three digits
+ * Numbers should be printed in ascending order, with three digits
+ * You can only use the putchar function (every other function (printf, puts, etc…) is forbidden)
+ * You can only use putchar six times maximum in your code
+ * You are not allowed to use any variable of type char
+ * All your code should be in the main functionNumbers must be separated by commas and a space.
+ * Return: 0
+ */
+int main(void)
+{
+	int i, j, k, l;
+
+	for (i = 0; i < 1000; i++)
+	{
+		j = i / 100;
+		k = (i / 10) % 10;
+		l = i % 10;
+
+		if (j < k && k < l)
+		{
+			putchar(j + '0');
+			putchar(k + '0');
+			putchar(l + '0');
+
+			if (i < 700)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
