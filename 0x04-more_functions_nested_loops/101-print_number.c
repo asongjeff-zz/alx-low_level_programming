bc80185e83cf4b171e8e6d5a077c676cd1cfaa31
asongjeff <asongjeff@gmail.com>
2021-09-02 19:43:35 +0100

    update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -5,40 +5,18 @@
  * @n: int type number
  * Description: Can only use _putchar to print.
  */
+
 void print_number(int n)
 {
+	unsigned int x;
 
-	int m, c, num;
-
-	num = n;
-
-	if (num < 0)
+	x = n;
+	if (n < 0)
 	{
-		num *= -1;
-		_putchar('-');
+		_putchar(45);
+		x = -n;
 	}
-
-	m = 1;
-	c = 1;
-	while (c)
-	{
-		if (num / (m * 10) > 0)
-			m *= 10;
-		else
-			c = 0;
-	}
-
-	while (num >= 0)
-	{
-		if (m == 1)
-		{
-			_putchar(num % 10 + '0');
-			num = -1;
-		}
-		else
-		{
-			_putchar((num / m % 10) + '0');
-			m /= 10;
-		}
-	}
+	if (x / 10)
+		print_number(x / 10);
+	_putchar((x % 10) + '0');
 }
