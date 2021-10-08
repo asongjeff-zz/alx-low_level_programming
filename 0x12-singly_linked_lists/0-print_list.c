1450c66bca1ae2d8da5f93a2b5a142f9dee473d3
asongjeff <asongjeff@gmail.com>
2021-10-08 01:11:02 +0100

    Updates

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,27 +1,19 @@
+#include <stdio.h>
 #include "lists.h"
+#include <stdlib.h>
 /**
-  *print_list - prints all the elements of a list_t.
-  *@h: pointer to list.
-  *
-  *Return: the number of nodes.
+  * print_list - prints all elements of a list_t list
+  * @h: singly linked list to print
+  * Return: number of nodes in the list
   */
 size_t print_list(const list_t *h)
 {
-	int count;
+	size_t i;
 
-	count = 0;
-	while (h != NULL)
+	for (i = 0; h; i++)
 	{
-		if (h->str == NULL)
-		{
-			printf("[0] (nil)\n");
-			h = h->next;
-			count++;
-		}
 		printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		count++;
 	}
-
-	return (count);
+	return (i);
 }
--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,19 +1,15 @@
 #include "lists.h"
+#include <stdlib.h>
+#include <stddef.h>
 /**
-  *free_list - frees a list_t
-  *@head: pointer to head of list.
-  *
-  *Return: 0.
+  * free_list - frees a linked list
+  * @head: the beginning of the list
   */
 void free_list(list_t *head)
 {
-	list_t *temp;
-
-	while (head != NULL)
-	{
-		temp = head;
-		free(temp->str);
-		head = head->next;
-		free(temp);
-	}
+	if (head == NULL)
+		return;
+	free_list(head->next);
+	free(head->str);
+	free(head);
 }
